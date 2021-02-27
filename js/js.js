eec06cbde4e94648cb8d7cf4a9737f67db216f71
nur165024 <nur42658@gmail.com>
2021-02-27 15:15:06 +0600

    latest products deisng update done

--- js/js.js
+++ js/js.js
@@ -17,11 +17,11 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d : " + hours + "h : "+ minutes + "m : " + seconds + "s ";
+  document.getElementById("timeCount").innerHTML = days + "d : " + hours + "h : "+ minutes + "m : " + seconds + "s ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("timeCount").innerHTML = "EXPIRED";
   }
 }, 1000);
